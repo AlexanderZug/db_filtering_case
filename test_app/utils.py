3ef13b0fbd2197d85a1d6d0d1f69ce12a2126228
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-07-31 23:03:55 +0300

    добавлен цикл для валидации

--- test_app/utils.py
+++ test_app/utils.py
@@ -1,68 +1,6 @@
 from test_app.models import Durations
 
 
-def result_to_output_all_data(query):
-    result = ''
-    for i in query:
-        result += (
-            f'Client: {i.client} '
-            f'| Equipment: {i.equipment} '
-            f'| Mode: {i.mode} '
-            f'| Minutes: {i.minutes} '
-            f'| Start: {i.start.strftime("%m/%d/%Y, %H")} '
-            f'| Stop: {i.stop.strftime("%m/%d/%Y, %H")}<br> '
-        )
-    return result
-
-
-def result_to_output_without_year(query):
-    result = ''
-    for i in query:
-        result += (
-            f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
-            f'| Minutes: {i.minutes} '
-            f'| Start: No data No data No data  No data '
-            f'| Stop: No data No data No data No data <br> '
-        )
-    return result
-
-
-def result_to_output_without_month(query):
-    result = ''
-    for i in query:
-        result += (
-            f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
-            f'| Minutes: {i.minutes} '
-            f'| Start: {i.start.strftime("%Y")} No data No data  No data '
-            f'| Stop: {i.stop.strftime("%Y")} No data No data  No data <br> '
-        )
-    return result
-
-
-def result_to_output_without_day(query):
-    result = ''
-    for i in query:
-        result += (
-            f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
-            f'| Minutes: {i.minutes} '
-            f'| Start: {i.start.strftime("%m/%Y")} No data  No data '
-            f'| Stop: {i.stop.strftime("%m/%Y")} No data  No data <br> '
-        )
-    return result
-
-
-def result_to_output_without_hour(query):
-    result = ''
-    for i in query:
-        result += (
-            f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
-            f'| Minutes: {i.minutes} '
-            f'| Start: {i.start.strftime("%m/%d/%Y")}  No data '
-            f'| Stop: {i.stop.strftime("%m/%d/%Y")}  No data <br> '
-        )
-    return result
-
-
 def filtered_time_query_set():
     db_set = {
         'query': lambda instance: Durations.filters_manager.select_related(
@@ -82,3 +20,78 @@ def filtered_time_query_set():
         ).db_query_without_year(instance),
     }
     return db_set
+
+
+class CheckQuerySet:
+    def __init__(self, query, key):
+        self.query = query
+        self.key = key
+
+    def __result_to_output_all_data(self):
+        result = ''
+        for i in self.query:
+            result += (
+                f'Client: {i.client} '
+                f'| Equipment: {i.equipment} '
+                f'| Mode: {i.mode} '
+                f'| Minutes: {i.minutes} '
+                f'| Start: {i.start.strftime("%m/%d/%Y, %H")} '
+                f'| Stop: {i.stop.strftime("%m/%d/%Y, %H")}<br> '
+            )
+        return result
+
+    def __result_to_output_without_year(self):
+        result = ''
+        for i in self.query:
+            result += (
+                f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
+                f'| Minutes: {i.minutes} '
+                f'| Start: No data No data No data  No data '
+                f'| Stop: No data No data No data No data <br> '
+            )
+        return result
+
+    def __result_to_output_without_month(self):
+        result = ''
+        for i in self.query:
+            result += (
+                f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
+                f'| Minutes: {i.minutes} '
+                f'| Start: {i.start.strftime("%Y")} No data No data  No data '
+                f'| Stop: {i.stop.strftime("%Y")} No data No data  No data <br> '
+            )
+        return result
+
+    def __result_to_output_without_day(self):
+        result = ''
+        for i in self.query:
+            result += (
+                f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
+                f'| Minutes: {i.minutes} '
+                f'| Start: {i.start.strftime("%m/%Y")} No data  No data '
+                f'| Stop: {i.stop.strftime("%m/%Y")} No data  No data <br> '
+            )
+        return result
+
+    def __result_to_output_without_hour(self):
+        result = ''
+        for i in self.query:
+            result += (
+                f'Client: {i.client} | Equipment: {i.equipment} | Mode: {i.mode} '
+                f'| Minutes: {i.minutes} '
+                f'| Start: {i.start.strftime("%m/%d/%Y")}  No data '
+                f'| Stop: {i.stop.strftime("%m/%d/%Y")}  No data <br> '
+            )
+        return result
+
+    def get_output(self):
+        if self.key == 'query':
+            return self.__result_to_output_all_data()
+        elif self.key == 'hour':
+            return self.__result_to_output_without_hour()
+        elif self.key == 'day':
+            return self.__result_to_output_without_day()
+        elif self.key == 'month':
+            return self.__result_to_output_without_month()
+        else:
+            return self.__result_to_output_without_year()
--- test_app/views.py
+++ test_app/views.py
@@ -4,14 +4,7 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView
 
 from test_app.forms import DurationsForm
-from test_app.utils import (
-    result_to_output_all_data,
-    result_to_output_without_day,
-    result_to_output_without_hour,
-    filtered_time_query_set,
-    result_to_output_without_month,
-    result_to_output_without_year,
-)
+from test_app.utils import CheckQuerySet, filtered_time_query_set
 
 
 class IndexView(CreateView):
@@ -21,39 +14,12 @@ class IndexView(CreateView):
 
     def form_valid(self, form):
         instance = form.save(commit=False)
-        if filtered_time_query_set()['query'](instance):
-            messages.success(
-                self.request,
-                result_to_output_all_data(
-                    filtered_time_query_set()['query'](instance)
-                ),
-            )
-        elif filtered_time_query_set()['hour'](instance):
-            messages.success(
-                self.request,
-                result_to_output_without_hour(
-                    filtered_time_query_set()['hour'](instance)
-                ),
-            )
-        elif filtered_time_query_set()['day'](instance):
-            messages.success(
-                self.request,
-                result_to_output_without_day(
-                    filtered_time_query_set()['day'](instance)
-                ),
-            )
-        elif filtered_time_query_set()['month'](instance):
-            messages.success(
-                self.request,
-                result_to_output_without_month(
-                    filtered_time_query_set()['month'](instance)
-                ),
-            )
-        else:
-            messages.success(
-                self.request,
-                result_to_output_without_year(
-                    filtered_time_query_set()['year'](instance)
-                ),
-            )
+        for k, i in filtered_time_query_set().items():
+            if i(instance):
+                messages.success(
+                    self.request,
+                    CheckQuerySet(i(instance), k).get_output(),
+                )
+                break
+        messages.error(self.request, 'Запрашиваемых данных не найдено')
         return HttpResponseRedirect(reverse('test_app:index'))
